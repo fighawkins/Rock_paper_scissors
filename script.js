7d80721cad0a20e1cb8f05eaa6b436aa7eee193d
Fig Hawkins <fig.hawkins@pcc.edu>
2023-09-02 16:09:01 -0700

    updated working draft of playRound function added

--- script.js
+++ script.js
@@ -7,21 +7,39 @@ return randomElement;
 
 }
 
-function checkWinner(playerSelection, computerSelection);{
+function checkWinner(playerSelection, computerSelection) {
     if(playerSelection === computerSelection) {
-        return "Tie!";
+        return "Tie";
     }
 
     else if(
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-         (playerSelection === "paper" && computerSelection === "rock") ||
-         (playerSelection === "scissors" && computerSelection === "paper")
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+         (playerSelection === "Paper" && computerSelection === "Rock") ||
+         (playerSelection === "Scissors" && computerSelection === "Paper")
     
     ){
-        return "Player Wins!";
+        return "Player";
     }
 
     else {
-        return "Computer Wins!";
+        return "Computer";
     }
 }
+
+function playRound(playerSelection, computerSelection){
+const result = checkWinner(playerSelection,computerSelection);
+if (result == "Tie"){
+    return "It's a tie!"
+}
+else if (result == "Player"){
+    return `You Win! ${playerSelection} beats ${computerSelection}`
+
+}
+else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`
+}
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
